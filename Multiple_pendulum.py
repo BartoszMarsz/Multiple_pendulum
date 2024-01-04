29d345a7574b05b6b35b4a49f0192682543ee293
BartoszMarsz <marsz.bartosz@gmail.com>
2024-01-04 16:22:28 +0100

    Kolejne poprawki

--- Multiple_pendulum.py
+++ Multiple_pendulum.py
@@ -1,26 +1,26 @@
 import mpmath as mp
 
-mp.mp.dps = 50        # number of digits
+mp.mp.dps = 50  # number of digits
 
 # Simulation parameters
-g = mp.mpf('9.81')    # standard gravity !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-time = 1              # time of simulation in seconds
-h0 = mp.mpf('0.01')   # time step
-eta = 0.01            # precision of energy conservation[%]
-N = 5                 # number of stages
-t = 0
+g = mp.mpf('9.81')  # standard gravity !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+time = 1  # time of simulation in seconds
+h0 = mp.mpf('0.01')  # default time step
+eta = 0.01  # precision of energy conservation[%]
+N = 5  # number of pendulum stages
+t = mp.mpf('0')
 
 # Declaration of pendulum stages parameters
-L = mp.matrix(N, 1)   # vector of pendulum lengths
-M = mp.matrix(N, 1)   # vector of masses
-Th = mp.matrix(N, 1)  # vector of angles
-Om = mp.matrix(N, 1)  # vector of angular velocities
+L = mp.matrix(N, 1)  # vector of pendulum lengths (2-dim, vertical)
+M = mp.matrix(N, 1)  # vector of masses (2-dim, vertical)
+Th = mp.matrix(N, 1)  # vector of angles (2-dim, vertical)
+Om = mp.matrix(N, 1)  # vector of angular velocities (2-dim, vertical)
 
 # Definition of pendulum stages parameters
 for i in range(N):
-    L[i, 0] = mp.mpf(1.0)/mp.mpf(N)
-    M[i, 0] = mp.mpf(1.0)/mp.mpf(N)
-    Th[i, 0] = mp.mpf(mp.radians(90+(90/(N-1))*i))
+    L[i, 0] = mp.mpf(1.0) / mp.mpf(N)
+    M[i, 0] = mp.mpf(1.0) / mp.mpf(N)
+    Th[i, 0] = mp.mpf(mp.radians(90 + (90 / (N - 1)) * i))
     Om[i, 0] = mp.mpf(0)
 
 
@@ -31,10 +31,11 @@ def T_energy(Th, Om):
     :return: Kinetic energy
     """
     T = mp.mpf('0')
-    for n in range(1,N+1):
-        for i in range(1,n+1):
-            for j in range(1,n+1):
-                T = T + mp.mpf('0.5') * M[n-1,0] * L[i-1,0] * L[j-1,0] * Om[i-1,0] * Om[j-1,0] * mp.cos(Th[i-1,0]-Th[j-1,0])
+    for n in range(1, N + 1):
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                T = T + mp.mpf('0.5') * M[n - 1, 0] * L[i - 1, 0] * L[j - 1, 0] * Om[i - 1, 0] * Om[j - 1, 0] * mp.cos(
+                    Th[i - 1, 0] - Th[j - 1, 0])
     return T
 
 
@@ -45,16 +46,16 @@ def V_energy(Th, Om):
     :return: Potential energy
     """
     V = mp.mpf('0')
-    for n in range(1,N+1):
-        for i in range(1,n+1):
-            V = V - g * M[n-1,0] * L[i-1,0] * mp.cos(Th[i-1,0])
+    for n in range(1, N + 1):
+        for i in range(1, n + 1):
+            V = V - g * M[n - 1, 0] * L[i - 1, 0] * mp.cos(Th[i - 1, 0])
     return V
 
 
 # Initial energy
-T0 = T_energy(Th, Om)
-V0 = V_energy(Th, Om)
-E0 = T0+V0
+T0 = T_energy(Th, Om)  # kinetic energy
+V0 = V_energy(Th, Om)  # potential energy
+E0 = T0 + V0  # total energy
 
 
 def diff_energy(Th, Om):
@@ -63,7 +64,7 @@ def diff_energy(Th, Om):
     :param Om: Vector of angular velocities
     :return: Difference in energy/E0
     """
-    return abs(100*((V_energy(Th, Om)+T_energy(Th, Om)-E0)/E0))
+    return abs(100 * ((V_energy(Th, Om) + T_energy(Th, Om) - E0) / E0))
 
 
 def F(Th, Om):
@@ -76,26 +77,27 @@ def F(Th, Om):
     B = mp.zeros(N)
     Om_2 = mp.zeros(N, 1)
     C = mp.zeros(N, 1)
-    #eq (2.25)
+    # eq (2.25)
     for i in range(N):
-        Om_2[i,0] = Om[i,0] ** 2
+        Om_2[i, 0] = Om[i, 0] ** 2
 
     for k in range(N):
         mass_sum = mp.mpf('0')
-        for n in range(k,N):
-            mass_sum = mass_sum + M[n,0]
-        #eq (2.25)
+        for n in range(k, N):
+            mass_sum = mass_sum + M[n, 0]
+        # eq (2.25)
         C[k, 0] = -g * mp.sin(Th[k, 0]) * mass_sum
         for i in range(N):
             if i > k:
                 mass_sum = mp.mpf('0')
                 for n in range(i, N):
-                    mass_sum = mass_sum + M[n,0]
-            #eq (2.26), (2.27)
+                    mass_sum = mass_sum + M[n, 0]
+            # eq (2.26), (2.27)
             A[k, i] = L[i, 0] * mp.cos(Th[i, 0] - Th[k, 0]) * mass_sum
             B[k, i] = L[i, 0] * mp.sin(Th[i, 0] - Th[k, 0]) * mass_sum
 
-    return (A ** -1) * ((B * Om_2) + C) #eq (2.24)
+    return (A ** -1) * ((B * Om_2) + C)  # eq (2.24)
+
 
 def Runge_Kutta(Th, Om, h):
     """
@@ -104,7 +106,7 @@ def Runge_Kutta(Th, Om, h):
     :param h: Step
     :return: Vectors of angles and velocities after step
     """
-    #eq (3.17)-(3.24)
+    # eq (3.17)-(3.24)
     K1_Th = Om
     K1_Om = F(Th, Om)
     K2_Th = Om + mp.mpf('0.5') * h * K1_Om
@@ -114,7 +116,7 @@ def Runge_Kutta(Th, Om, h):
     K4_Th = Om + h * K3_Om
     K4_Om = F(Th + h * K3_Th, Om + h * K3_Om)
 
-    #eq (3.25), (3.26)
+    # eq (3.25), (3.26)
     return Th + mp.mpf('1/6') * h * (K1_Th + mp.mpf('2') * K2_Th +
                                      mp.mpf('2') * K3_Th + K4_Th), \
            Om + mp.mpf('1/6') * h * (K1_Om + mp.mpf('2') * K2_Om +
@@ -127,56 +129,56 @@ DATA.write('Time= ' + str(time) + '\n')
 DATA.write('Step= ' + str(h0) + '\n')
 DATA.write('Number_of_stages= ' + str(N) + '\n')
 for i in range(N):
-    DATA.write('Length_of_' + str(i+1) + '_stage= ' + str(L[i,0]) + '\n')
+    DATA.write('Length_of_' + str(i + 1) + '_stage= ' + str(L[i, 0]) + '\n')
 for i in range(N):
-    DATA.write('Mass_of_' + str(i+1) + '_mass= ' + str(M[i,0]) + '\n')
+    DATA.write('Mass_of_' + str(i + 1) + '_mass= ' + str(M[i, 0]) + '\n')
 DATA.close()
 
 # Save initial data
 DATA = open('raw_data.pdb', 'w')
 DATA.write(str(0))
 for i in range(N):
-    DATA.write(' ' + str(round(Th[i,0],5)))
+    DATA.write(' ' + str(round(Th[i, 0], 5)))
 for i in range(N):
-    DATA.write(' ' + str(round(Om[i,0],5)))
-DATA.write(' ' + str(round(T_energy(Th, Om),5)))
-DATA.write(' ' + str(round(V_energy(Th, Om),5)))
-DATA.write(' ' + str(diff_energy(Th,Om)))
+    DATA.write(' ' + str(round(Om[i, 0], 5)))
+DATA.write(' ' + str(round(T_energy(Th, Om), 5)))
+DATA.write(' ' + str(round(V_energy(Th, Om), 5)))
+DATA.write(' ' + str(diff_energy(Th, Om)))
 DATA.write(' ' + str(h0))
 DATA.write('\n')
 
 # Simulation loop
-while(t < time + h0):
-    print('t = '+str(round(t,5)), end='    ')
+while t < time + h0:
+    print('t = ' + str(round(t, 5)), end='    ')
 
     # Calculation of angles and angular velocities after time step
-    h = h0 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    h = h0  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     Th_1, Om_1 = Runge_Kutta(Th, Om, h)
     dE = diff_energy(Th_1, Om_1)
 
     # Condition of energy conservation
-    while(dE > eta):
-        h = mp.mpf('0.1')*h
+    while dE > eta:
+        h = mp.mpf('0.1') * h
         Th_1, Om_1 = Runge_Kutta(Th, Om, h)
-        dE = diff_energy(Th_1, Om_1) # Obliczenie tylko dE!!!!!!!!!!!!!!!!!!
+        dE = diff_energy(Th_1, Om_1)  # Obliczenie tylko dE!!!!!!!!!!!!!!!!!!
     print('dE = ' + str(round(dE, 5)), end='    ')
     print('h = ' + str(h))
     Th, Om = Th_1, Om_1
-    T = T_energy(Th, Om) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    V = V_energy(Th, Om) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    T = T_energy(Th, Om)  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    V = V_energy(Th, Om)  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     # Write data to file
-    DATA.write(str(round(t,8)))
+    DATA.write(str(round(t, 8)))
     for j in range(N):
-        DATA.write(' ' + str(round(Th[j,0],5)))
+        DATA.write(' ' + str(round(Th[j, 0], 5)))
     for j in range(N):
-        DATA.write(' ' + str(round(Om[j,0],5)))
-    DATA.write(' ' + str(round(T,5)))
-    DATA.write(' ' + str(round(V,5)))
+        DATA.write(' ' + str(round(Om[j, 0], 5)))
+    DATA.write(' ' + str(round(T, 5)))
+    DATA.write(' ' + str(round(V, 5)))
     DATA.write(' ' + str(dE))
     DATA.write(' ' + str(h))
     DATA.write('\n')
 
-    # Time
-    t = t+h
+    # Time iteration
+    t = t + h
 DATA.close()
--- Plots.py
+++ Plots.py
@@ -3,29 +3,28 @@ import numpy as np
 from matplotlib.animation import FuncAnimation
 from matplotlib.animation import FFMpegWriter
 
-params = np.loadtxt('5_stages_bigmass/params.pdb', delimiter=' ', usecols=1)
-time = float(params[0])
-h = float(params[1])
-N = int(params[2])
-L = params[3:3+N]
-M = params[3+N:3+2*N]
+params = np.loadtxt('params.pdb', delimiter=' ', usecols=1)
+time = float(params[0])  # time of simulation in seconds
+h = float(params[1])  # default time step
+N = int(params[2])  # number of pendulum stages
+L = params[3:3 + N]  # pendulum lengths (vector, 1-dim)
+M = params[3 + N:3 + 2 * N]  # masses (vector, 1-dim)
 
+data = np.loadtxt('data.pdb', delimiter=' ')
+t = data[:, 0]  # time iterators (vector, 1-dim)
+Th = data[:, 1:1 + N]  # matrix of angles (row: th_1, ..., th_N, col: evolution in time)
+Om = data[:, 1 + N:1 + 2 * N]  # matrix of angular velocities (row: om_1, ..., om_N, col: evolution in time)
+T_data = data[:, 1 + 2 * N]  # kinetic energy evolution in time (vector, 1-dim)
+V_data = data[:, 2 + 2 * N]  # potential energy evolution in time (vector, 1-dim)
+dE_data = data[:, 3 + 2 * N]  # difference in energy evolution in time (vector, 1-dim)
+tau = t.shape[0]  # Number of time frames
+X = np.zeros((tau, 1))  # position x of anchor point evolution in time (fixed) (vector, 2-dim, vertical)
+Y = np.zeros((tau, 1))  # position y of anchor point evolution in time (fixed) (vector, 2-dim, vertical)
 
+E_max = max(10, np.amax(np.concatenate((T_data, V_data))))  # max energy for axis limit (not less than 10)
+E_min = min(-10, np.amin(np.concatenate((T_data, V_data))))  # min energy for axis limit (not more than -10)
+dE_max = np.amax(dE_data)  # max difference in energy for axis limit
 
-data = np.loadtxt('5_stages_bigmass/data.pdb', delimiter=' ')
-t = data[:, 0]
-Th = data[:, 1:1 + N]
-Om = data[:, 1+N:1+2*N]
-T_data = data[:, 1+2*N]
-V_data = data[:, 2+2*N]
-dE_data = data[:, 3+2*N]
-tau = t.shape[0]
-X = np.zeros((tau,1))
-Y = np.zeros((tau,1))
-
-E_max = max(10,np.amax(np.concatenate((T_data,V_data))))
-E_min = min(-10,np.amin(np.concatenate((T_data,V_data))))
-dE_max = np.amax(dE_data)
 
 def sum_to(V, start, stop):
     """
@@ -35,21 +34,26 @@ def sum_to(V, start, stop):
     :return: Sum of elements from start to stop
     """
     sum = 0
-    for i in range(start, stop+1):
+    for i in range(start, stop + 1):
         sum = sum + V[i]
     return sum
 
+
+"""
 def XY_pos(k):
-    X_k = np.sum(L[0:k]*np.sin(Th[:, 0:k]), axis=1)
-    Y_k = - np.sum(L[0:k]*np.cos(Th[:, 0:k]), axis=1)
+    X_k = np.sum(L[0:k] * np.sin(Th[:, 0:k]), axis=1)
+    Y_k = - np.sum(L[0:k] * np.cos(Th[:, 0:k]), axis=1)
     return X_k, Y_k
+"""
 
-for i in range(1,N+1):
-    X = np.concatenate((X, np.sum(L[0:i]*np.sin(Th[:, 0:i]), axis=1, keepdims=True)), axis=1)
-    Y = np.concatenate((Y, - np.sum(L[0:i]*np.cos(Th[:, 0:i]), axis=1, keepdims=True)), axis=1)
+for i in range(1, N + 1):
+    X = np.concatenate((X, np.sum(L[0:i] * np.sin(Th[:, 0:i]), axis=1, keepdims=True)), axis=1)
+    Y = np.concatenate((Y, - np.sum(L[0:i] * np.cos(Th[:, 0:i]), axis=1, keepdims=True)), axis=1)
 
 
-X_k, Y_k = XY_pos(N)
+# X_k, Y_k = XY_pos(N)
+
+
 def static():
     fig = plt.figure(figsize=(12.8, 6.8))
     figManager = plt.get_current_fig_manager()
@@ -121,11 +125,12 @@ def static():
     plt.savefig('5_stages_bigmass/5_stages_bigmass.png', bbox_inches='tight')
     plt.show()
 
+
 def animation():
     # defining figure and plots
     fig = plt.figure(figsize=(12.8, 6.8))
-    figManager = plt.get_current_fig_manager()
-    #figManager.window.showMaximized()
+    # figManager = plt.get_current_fig_manager()
+    # figManager.window.showMaximized()
     ax1 = fig.add_subplot(2, 2, 1)
     ax2 = fig.add_subplot(2, 2, 3)
     ax3 = fig.add_subplot(2, 2, (2, 4))
@@ -171,13 +176,13 @@ def animation():
     trajectory, = ax3.plot(0, 0)
     line, = ax3.plot(0, 0)
     # timer
-    timer = ax3.text(0.05, 0.95, '', transform=ax3.transAxes, va='top', ha='left', color='black',fontsize=14, bbox=(dict(facecolor='wheat', boxstyle='round')))
+    timer = ax3.text(0.05, 0.95, '', transform=ax3.transAxes, va='top', ha='left', color='black', fontsize=14,
+                     bbox=(dict(facecolor='wheat', boxstyle='round')))
 
     # specifying objects appearance
     T.set_color('darkorange')
     V.set_color('mediumblue')
     dE.set_color('darkorange')
-
 
     trajectory.set_color('lightgray')
     trajectory.set_alpha(0.3)
@@ -190,27 +195,36 @@ def animation():
         T.set_data([t[0]], [T_data[0]])
         V.set_data([t[0]], [V_data[0]])
         dE.set_data([t[0]], [dE_data[0]])
-        trajectory.set_data([X[0, N]],[Y[0, N]])
+        trajectory.set_data([X[0, N]], [Y[0, N]])
         line.set_xdata(X[0, :])
         line.set_ydata(Y[0, :])
-        timer.set_text(str(0)+'s')
+        timer.set_text(str(0) + 's')
         return line, T, V, dE, trajectory, timer,
+
     def animation_frame(i):
-        print(round(i/60,2))
+        print(round(i / 60, 2))
         T.set_data(t[:i], T_data[:i])
         V.set_data(t[:i], V_data[:i])
         dE.set_data(t[:i], dE_data[:i])
-        trajectory.set_data(X[:i,N], Y[:i,N])
-        line.set_data(X[i,:],Y[i,:])
+        trajectory.set_data(X[:i, N], Y[:i, N])
+        line.set_data(X[i, :], Y[i, :])
         timer.set_text(format(t[i], '.2f'))
 
         return line, T, V, dE, trajectory, timer,
 
-    anim = FuncAnimation(fig, func=animation_frame, frames=tau, init_func=init, interval=int(1000/60), repeat=False, blit=True)
-    writervideo = FFMpegWriter(fps=60)
-    anim.save('5_stages_bigmass/Pendulum5.mp4', writer=writervideo)
-    #plt.show()
+    anim = FuncAnimation(fig, func=animation_frame, frames=tau, init_func=init, interval=int(1000 / 60), repeat=False,
+                         blit=True)
+    plt.show()
+    dec = input("Do you want to save?\ny - yes\n")
+    if dec == "y":
+        anim.save('Pendulum.mp4', writer=FFMpegWriter(fps=60))
     plt.close()
-#static()
-#animation()
 
+
+stat_anim = input("With do you want to display?\n1 - static, 2 - animation\n")
+if stat_anim == str(1):
+    static()
+elif stat_anim == str(2):
+    animation()
+else:
+    print("Bad value.")
